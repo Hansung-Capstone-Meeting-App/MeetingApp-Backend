b4f1397f6610e05bddc59530e345700f21e08883
likethisjm <wookjaelee777@naver.com>
2026-03-06 20:35:52 +0900

    feat: add calendar repositories and update config

---
+++ src/main/java/com/capston/demo/domain/calender/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.capston.demo.domain.calender.repository;
+import com.capston.demo.domain.calender.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByMeetingId(Long meetingId);
+    List<Event> findByWorkspaceId(Long workspaceId);
+}
